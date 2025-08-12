5b478137df3e55a24dfc6bd38bb317aa61d56369
morning-start <morning-start@foxmail.com>
2025-08-12 19:51:58 +0800

    refactor(source): 修改format_url以支持可选Source参数
    将format_url函数的source参数改为Option类型，并提供默认值GodotHub
    修改域名替换逻辑以使用GodotEngine的域名作为替换源

--- src/core/source.rs
+++ src/core/source.rs
@@ -12,7 +12,8 @@ impl Source {
     }
 }
 
-pub fn format_url(url: &str, source: Source) -> String {
+pub fn format_url(url: &str, source: Option<Source>) -> String {
+    let source = source.unwrap_or(Source::GodotHub);
     let domain = source.get_domain();
-    url.replace("github.com/godotengine", domain)
+    url.replace(Source::GodotEngine.get_domain(), domain)
 }
